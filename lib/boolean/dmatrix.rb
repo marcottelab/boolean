8e5a61d59efc80f225fb4af125c6fa52c0fcde08
John Woods <jwoods@vt.edu>
2013-06-06 17:52:37 -0500

    Fixed some typos mostly; inverted distances.

--- lib/boolean/dmatrix.rb
+++ lib/boolean/dmatrix.rb
@@ -6,7 +6,7 @@ module Boolean
 
     # Create a new distance matrix, for predicting to from from
     def initialize to, from
-      super(to.shape[0], from.shape[0], :float64)
+      super(:dense, [to.shape[0], from.shape[0]], :float64)
 
       (0...to.shape[0]).each do |i|
         m_set = to.orthogroups_for_phenotype(i)
@@ -14,7 +14,7 @@ module Boolean
         (0...from.shape[0]).each do |j|
           n_set = from.orthogroups_for_phenotype(j)
           k_set = m_set & n_set
-          self[i,j] = Distribution::Hypergeometric.cdf(k_set.size, m_set.size, n_set.size, hs_opm.shape[1])
+          self[i,j] = 1.0 - Distribution::Hypergeometric.cdf(k_set.size, m_set.size, n_set.size, to.shape[1])
         end
       end
     end
--- lib/run.rb
+++ lib/run.rb
@@ -13,5 +13,5 @@ dr_opm = dr_gpm.opmatrix(reader) # zebrafish orthogroup-phenotype matrix
 dr_bopm = Boolean::BOPMatrix.new(dr_opm, :|)
 
 # Create dense matrix, don't initialize values by offering a default -- we're going to do so momentarily.
-distances = DMatrix.new(hs_opm, dr_bopm)
+distances = Boolean::DMatrix.new(hs_opm, dr_bopm)
 
