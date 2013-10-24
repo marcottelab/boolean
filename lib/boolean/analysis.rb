e84033d51422ce5b64fcb9d3510933bf1ef9796d
John Woods <jwoods@vt.edu>
2013-10-24 16:31:50 -0500

    Fixed minor inspect bug

--- lib/boolean/analysis.rb
+++ lib/boolean/analysis.rb
@@ -2,7 +2,7 @@ module Boolean
   class Analysis
 
     def inspect #:nodoc:
-      ["#<Boolean::OrthoReader:0x00#{(self.object_id << 1).to_s(16)}", "@to @to_gpm @from @from_gpm", "distances=#{@distances.inspect}", "op=:#{@op.to_s}", "reader=#{@reader.inspect}"].join(' ') + " >"
+      ["#<Boolean::Analysis:0x00#{(self.object_id << 1).to_s(16)}", "@to @to_gpm @from @from_gpm", "distances=#{@distances.inspect}", "op=:#{@op.to_s}", "reader=#{@reader.inspect}"].join(' ') + " >"
     end
 
 
